2c8e05fbdb35c87c171376ba2aad109636ec4a46
Ash Wilson <smashwilson@gmail.com>
2014-11-20 11:25:31 -0500

    Separate image building from running.

---
+++ docker/build/vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Set variables that are useful for building this docker container. The actual building is done by
+# script/dockbuild.
+
+export TAG=devsite/build
+export CONTEXT=${ROOT}
